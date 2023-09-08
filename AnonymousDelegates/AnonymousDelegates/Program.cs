2d0ae971c9734a26f7b3630147d52e168beac7e1
Dawood Amjad <dawoodamjad1997@gmail.com>
2023-09-08 16:55:22 +0500

    adds AnonymousDelegat Practice

---
+++ AnonymousDelegates/AnonymousDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnonymousDelegates
+{
+    public delegate string MyDelegate(int arg1, int arg2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate f = delegate (int arg1, int arg2)
+            {
+                return (arg1 * arg2).ToString();
+            };
+            Console.WriteLine("Result: "+ f(10,29));
+        }
+    }
+}
