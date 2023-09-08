2c53f442c4a0e80c7c9fd8b16a11435455a26ddc
Dawood Amjad <dawoodamjad1997@gmail.com>
2023-09-08 16:31:16 +0500

    adds basicDelegates Practice

---
+++ BasicDelegates/BasicDelegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BasicDelegates
+{
+    // TODO: declare the delegate type
+    public delegate string MyDelegate(int arg1, int arg2);
+
+    class MyClass
+    {
+        // Delegates can be bound to instance members as well as
+        // static class functions
+        public string instanceMethod1(int arg1, int arg2)
+        {
+            return ((arg1 + arg2) * arg1).ToString();
+        }
+    }
+
+    class Program
+    {
+        // TODO: Create functions to serve as delegate implementations
+        static string func1(int a, int b)
+        {
+            return(a + b).ToString();
+        }
+        static string func2(int a, int b)
+        {
+            return (a*b).ToString();
+        }
+        static void Main(string[] args)
+        {
+            // TODO: exercise each delegate function
+
+            MyDelegate f = func1;
+            Console.WriteLine("addition result: " + f(10,20));
+            f  = func2;
+            Console.WriteLine("multiplication result: " + f(10, 20));
+            // TODO: Use an instance function of a class as a delegate
+
+            MyClass myClass = new MyClass();
+            f = myClass.instanceMethod1;
+            Console.WriteLine("instance of a class result" + f(10, 20));
+        }
+    }
+}
